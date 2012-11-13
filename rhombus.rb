edfa94dfc7ed27d3649630966313c73a1f8991d0
zuikov-yuriy <yuriy@zuikov.org.ua>
2012-11-13 12:54:07 +0200

    spec Rhombus  add

---
+++ rhombus.rb
@@ -0,0 +1,15 @@
+class Rhombus < Parallelogram
+
+  def initialize(side)
+    super
+  end
+
+  def perimeter
+    @perimeter = 4*@side[0]
+  end
+
+  def area
+    @area = @side[0]*@side[1]
+  end
+
+end
