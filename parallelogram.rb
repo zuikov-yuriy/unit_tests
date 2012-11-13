d4f68258959a0c40922287bb372275b0b75e0153
zuikov-yuriy <yuriy@zuikov.org.ua>
2012-11-13 12:47:51 +0200

    class Parallelogram  add

---
+++ parallelogram.rb
@@ -0,0 +1,14 @@
+class Parallelogram < Trapeze
+
+  def initialize(side)
+    super
+  end
+
+  def perimeter
+    @perimeter = 2*(@side[0]+@side[1])
+  end
+
+  def area
+    @area = @side[0]*@side[2]
+  end
+end
