db910c83429f73115055b6d04d1f8a94bf964886
zuikov-yuriy <yuriy@zuikov.org.ua>
2012-11-13 12:45:29 +0200

    spec Parallelogram  add

---
+++ spec/parallelogram_spec.rb
@@ -0,0 +1,28 @@
+require '../figurewithangle'
+require '../trapeze'
+require 'spec_helper'
+
+
+describe Parallelogram do
+
+  let(:p) { Parallelogram.new([10,15,5])}
+
+  describe "#initialize" do
+    it "should be initialized as Trapeze" do
+      Parallelogram.new([10,15,5]).should be_true
+    end
+  end
+
+  describe "#perimeter" do
+    it "should return sum all sides " do
+      p.perimeter.should == 50
+    end
+  end
+
+  describe "#area" do
+    it "should return area figure" do
+      p.area.should == 50
+    end
+  end
+
+end
